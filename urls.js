ec931446fe4c173b2875cf039ef4bde2b225ba77
Tarmana <31622729+Tarmana@users.noreply.github.com>
2024-08-13 08:55:34 +0700

    Create urls.js

---
+++ urls.js
@@ -0,0 +1,22 @@
+var urls = {
+    't1': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3D7sGdPgRScUM&sa=D&source=editors&ust=1722458918255036&usg=AOvVaw1dun_RwtK6YaCx7HKSHD3V',
+    't2': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DoiFGd102Ep8&sa=D&source=editors&ust=1722458918253010&usg=AOvVaw3rwLkrW6eMIikP1_o06Nq6',
+    't3': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DXkdnbnRg0QY&sa=D&source=editors&ust=1722458918253219&usg=AOvVaw3vrga0M0qD_k4VT5PESz4h',
+    't4': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DExLU87w4_1I&sa=D&source=editors&ust=1722459756409188&usg=AOvVaw0Ighen6mGHuZYPIhnyfy9q',
+    't5': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DpLkoLIKllls&sa=D&source=editors&ust=1722460328695994&usg=AOvVaw3k7oUmpEcLJDRq3emh8-Dw',
+    't6': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DE_WcxbMBMdU&sa=D&source=editors&ust=1722460328694818&usg=AOvVaw1D0VIMeUvkRq3EMTHwT5TZ',
+    't7': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DVpBDi5GYuu4&sa=D&source=editors&ust=1722460756820222&usg=AOvVaw2Kr6LjXlmL8oGbaioUfa7R',
+    't8': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DWtiDVrhiuhM&sa=D&source=editors&ust=1722460756820342&usg=AOvVaw2arUeyUAbRFvvInOzx36xW',
+    't9': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DBWVVff_wwKg&sa=D&source=editors&ust=1722460756821336&usg=AOvVaw0IGXGMkF1EmtB6bQATaY1Q',
+    't10': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DBWVVff_wwKg&sa=D&source=editors&ust=1722508425119626&usg=AOvVaw1FENskhJ45o5awunMCnSBL',
+    't11': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DZfDaxz-MGRs&sa=D&source=editors&ust=1722508425119950&usg=AOvVaw38kHv423wzOZfYmi4S2pno',
+    't12': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3Dws4LASFQzCQ&sa=D&source=editors&ust=1722508425120685&usg=AOvVaw00lPHHoWBzKUAeZiMZXWZw',
+    't13': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DINx8dB21jsQ&sa=D&source=editors&ust=1722508425121369&usg=AOvVaw0QjHLVjIg5R2IS1oA6LH5E',
+    't14': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DQYi36DHRWfE&sa=D&source=editors&ust=1722508425121584&usg=AOvVaw3CoxRts24Kaf_qBbdsKnWM',
+    't15': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3D2QinLQ2ZlfE&sa=D&source=editors&ust=1722508599199644&usg=AOvVaw1DloGVfkbm0HTHb5_4Rcxb',
+    't16': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3D0zZU0u_b4m8&sa=D&source=editors&ust=1722508599199830&usg=AOvVaw3XMuzr35ftYBsLoHgLeLUF',
+    't17': 'https://www.google.com/url?q=https://www.youtube.com/watch?v%3DAMa0EjH08KE&sa=D&source=editors&ust=1722524831335397&usg=AOvVaw3SP04M2BVScOV9CTErCgYC',
+    't18': 'https://l.facebook.com/l.php?u=https://www.youtube.com/results?search_query=skillshare+coupon+code',
+    'tb1': 'https://l.facebook.com/l.php?u=https://www.youtube.com/results?search_query=skillshare+coupon',
+    'twitter': 'https://twitter.com/shoaiybsysa'
+};
